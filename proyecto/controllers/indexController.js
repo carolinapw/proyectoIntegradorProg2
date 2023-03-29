d3c27fe6aa9eb50bceb8dbce5522e07fc9ead340
justinacademartori <mcademartori@udesa.edu.ar>
2023-03-29 16:23:20 -0300

    mvc de index

---
+++ proyecto/controllers/indexController.js
@@ -0,0 +1,15 @@
+let productsDb = require("../db/productsDb");
+
+let indexController = {
+    descripcion: function (req,res){
+        return res.send (productsDb)
+    },
+    index: function (req, res){
+        return res.send ("hola home")
+    },
+    
+}
+
+
+
+module.exports = indexController;
