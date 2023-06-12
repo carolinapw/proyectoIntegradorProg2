e503b2f32e257059c086368a8d42562169f74687
justinacademartori <mcademartori@udesa.edu.ar>
2023-06-12 13:33:55 -0300

    ruteo de productsController

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -35,26 +35,15 @@ let productsController = {
             .catch(function(error) {
                 console.log(error);
             })
-
-        // if (idRuta < db.productos.length) {
-        //     for (let i = 0; i < db.productos.length; i++) {          
-        //         if (idRuta == db.productos[i].id) {
-        //             resultados.push(db.productos[i]);
-        //         }
-        //     }
-            
-        // }
-        // return res.render('product', {
-        //     productsDb: resultados[0],
-        //     commentsDb: db.comentarios
-        // })
-        
     },
     showEdit: function (req, res) {
         return res.render('product-add', {
             productsDb: db.productos,
             infoUsuario: db.usuario
         });
+    },
+    edit: function(req,res) {
+
     },
     nuevoProducto: function (req, res) {
         let errores = {message: ''}
@@ -68,18 +57,10 @@ let productsController = {
         //   errores.message = errores.message + 'Agregar imagen';
         // }
         
-    }
-    // comment: function (req, res) {
-        
-    //     return res.render('product', {commentsDb: db.comentarios})
-        
-    // },
-    // borrar: function (req, res) {
-    //     return res.render('product-add')
-    // },
-    // agregar: function (req, res) {
-    //     return res.render('product-add');
-    // }
+    },
+    borrar: function (req, res) {
+        return res.render('product-add')
+    },
 }
 
 
